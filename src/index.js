f0c161ec9d7683b0c6d20a7cf9c8d1cf114ce324
丹侠 <danxia.shidx@alipay.com>
2015-11-18 11:36:13 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * 这是蚂蚁金服基于 Facebook 的 react-native 开发工具包 提供一套镜像工具，
+ * 并在官方基础上提供额外的打包参数
+ * @author 丹侠 danxia.shidx@alipay.com
+ */
+
+'use strict';
+
+var bundle = function (){
+  console.log('bundle!');
+};
+
+module.exports = {
+  bundle: bundle
+};
