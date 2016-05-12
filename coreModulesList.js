aafc85ad43068104d790fe1857fe12d5428c846f
Denis <swainet@126.com>
2016-05-12 11:15:55 +0800

    更新模块名称

--- coreModulesList.js
+++ coreModulesList.js
@@ -132,6 +132,7 @@ const coreModulesList = [
   "ReactCurrentOwner",
   "ReactDefaultBatchingStrategy",
   "ReactDefaultPerf",
+  "ReactElement",
   "ReactElementValidator",
   "ReactEventEmitterMixin",
   "ReactFragment",
